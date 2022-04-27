99f74524c1844734812ce3d57a3a0d51739fc849
Elias <eliasventura23@gmail.com>
2022-04-27 09:01:00 -0400

    got some info from api on the page

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 
-    fetch('https://pokeapi.co/api/v2/pokemon/35')
+    fetch('https://pokeapi.co/api/v2/pokemon/1')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => renderPokemon(data))
+
+    function renderPokemon(pokemon) {
+        let img= document.querySelector("#pokemon-a-img")
+        
+        img.src = (pokemon['sprites']['back_default'])
+        console.log(img)
+        // document.querySelector("#pokemon-a-img").src = (pokemon['sprites']['back_default:'])
+    }
     
