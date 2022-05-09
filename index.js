0e13299fc0d0d52e1788ddca90912bd62e93bf61
Elias <eliasventura23@gmail.com>
2022-05-09 08:21:37 -0400

    added dropdown of names

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 
-    fetch('https://pokeapi.co/api/v2/pokemon/1')
+    fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=1126')
     .then(res => res.json())
-    .then(data => renderPokemon(data))
+    .then(data => renderAllPokemonNames(data))
 
-    function renderPokemon(pokemon) {
-        let img= document.querySelector("#pokemon-a-img")
-        
-        img.src = (pokemon['sprites']['back_default'])
-        console.log(img)
-        // document.querySelector("#pokemon-a-img").src = (pokemon['sprites']['back_default:'])
+
+    function renderAllPokemonNames(data) {
+        let pokemons = data["results"]
+        for (const pokemon of pokemons) {
+            let list = document.querySelector("#pokemon-a-dropdown")
+            let option = document.createElement('option')
+            option.innerHTML = pokemon.name
+            list.append(option)
+            console.log(list)
+        }
+        console.log(data["results"])
     }
+
+    // function renderPokemon(pokemon) {
+    //     let img= document.querySelector("#pokemon-a-img")
+        
+    //     img.src = (pokemon['sprites']['back_default'])
+    //     console.log(img)
+    //     // document.querySelector("#pokemon-a-img").src = (pokemon['sprites']['back_default:'])
+    // }
     
