e181b73c6640cd5f7759bd5c3561b974d16a60ac
Elias <eliasventura23@gmail.com>
2022-05-09 08:44:08 -0400

    add event listener to dropdown list'

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let list = document.querySelector("#pokemon-a-dropdown")
+
 
     fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=1126')
     .then(res => res.json())
@@ -7,14 +9,19 @@
     function renderAllPokemonNames(data) {
         let pokemons = data["results"]
         for (const pokemon of pokemons) {
-            let list = document.querySelector("#pokemon-a-dropdown")
+            
             let option = document.createElement('option')
             option.innerHTML = pokemon.name
+            
             list.append(option)
-            console.log(list)
+            
+            
         }
-        console.log(data["results"])
     }
+
+    list.addEventListener('change', function (event) {
+        console.log(event.target)
+    })
 
     // function renderPokemon(pokemon) {
     //     let img= document.querySelector("#pokemon-a-img")
